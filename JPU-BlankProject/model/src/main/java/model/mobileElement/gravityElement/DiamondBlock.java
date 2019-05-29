f41903d6dd38e6d188c9c9cecf583a0ed560cffd
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2019-05-29 10:53:51 +0200

    Create RockBall and DiamondBlock class from GravityElement

---
+++ JPU-BlankProject/model/src/main/java/model/mobileElement/gravityElement/DiamondBlock.java
@@ -0,0 +1,27 @@
+package model.mobileElement.gravityElement;
+
+import model.Sprite;
+import model.State;
+
+
+/**
+ * <h1>The DiamondBlock Class.</h1>
+ *
+ * @author Gabriel RICARD
+ * @version 0.1
+ */
+public class DiamondBlock extends GravityElement {
+	
+	
+	/**
+	 * The State preset for a DiamondBlock Element.
+	 */
+	private static State state = State.LOOTABLE;
+	
+	/**
+	 * The Sprite preset for a DiamondBlock Element.
+	 */
+	private static Sprite sprite;
+	
+
+}
---
+++ JPU-BlankProject/model/src/main/java/model/mobileElement/gravityElement/RockBall.java
@@ -0,0 +1,27 @@
+package model.mobileElement.gravityElement;
+
+import model.Sprite;
+import model.State;
+
+
+/**
+ * <h1>The RockBall Class.</h1>
+ *
+ * @author Gabriel RICARD
+ * @version 0.1
+ */
+public class RockBall extends GravityElement {
+	
+	
+	/**
+	 * The State preset for a RockBall Element.
+	 */
+	private static State state = State.UNBREAKABLE;
+	
+	/**
+	 * The Sprite preset for a Rockball Element.
+	 */
+	private static Sprite sprite;
+	
+	
+}
