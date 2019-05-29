77c9381cd30ad2eb71625ceb0d4a45d3f2c3516f
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2019-05-29 09:46:15 +0200

    Create Player Element class

---
+++ JPU-BlankProject/model/src/main/java/model/mobileElement/Player.java
@@ -0,0 +1,71 @@
+package model.mobileElement;
+
+
+
+public class Player extends MobileElement{
+	
+	/**
+	 * Verify if moving up is possible.
+	 * 
+	 * @return true if the move is possible or false if the move is not possible
+	 */
+	public boolean isMoveUpPossible() {
+		return false;
+	}
+	
+	/**
+	 * Move the player Element to the upper Element
+	 */
+	public void movePlayerUp() {
+		
+	}
+	
+	/**
+	 * Verify if moving down is possible.
+	 * 
+	 * @return true if the move is possible or false if the move is not possible
+	 */
+	public boolean isMoveDownPossible() {
+		return false;
+	}
+	
+	/**
+	 * Move the player Element to the lower Element
+	 */
+	public boolean movePlayerDown() {
+		
+	}
+	
+	/**
+	 * Verify if moving left is possible.
+	 * 
+	 * @return true if the move is possible or false if the move is not possible
+	 */
+	public boolean isMoveLeftPossible() {
+		return false;
+	}
+	
+	/**
+	 * Move the player Element to the left Element
+	 */
+	public void movePlayerLeft() {
+		
+	}
+	
+	/**
+	 * Verify if moving right is possible.
+	 * 
+	 * @return true if the move is possible or false if the move is not possible
+	 */
+	public boolean isMoveRightPossible() {
+		return false;
+	}
+	
+	/**
+	 * Move the player Element to the right Element
+	 */
+	public void movePlayerRight() {
+		
+	}
+
+}
