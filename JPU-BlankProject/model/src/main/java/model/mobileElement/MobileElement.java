04dfe1b4108cd39cc24cc6e7e3173e11b65d2117
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2019-05-29 09:29:35 +0200

    Create MobileElement Class and model.mobileElement sub-package

---
+++ JPU-BlankProject/model/src/main/java/model/mobileElement/MobileElement.java
@@ -0,0 +1,43 @@
+package model.mobileElement;
+
+
+public abstract class MobileElement extends Element {
+	
+	/**
+	 * Move the Element to the upper Element (x, y+1) and replace old Element by an Empty Element.
+	 * 
+	 * @param movingElement is the type of Element which is moving
+	 */
+	protected void MoveUp(MobileElement movingElement) {
+		
+	}
+	
+	/**
+	 * Move the Element to the lower Element (x, y-1) and replace old Element by an Empty Element.
+	 * 
+	 * @param movingElement is the type of Element which is moving
+	 */
+	protected void MoveDown(MobileElement movingElement) {
+		
+	}
+	
+	/**
+	 * Move the Element to the upper Element (x, y+1) and replace old Element by an Empty Element.
+	 * 
+	 * @param movingElement is the type of Element which is moving
+	 */
+	protected void MoveRight(MobileElement movingElement) {
+		
+	}
+	
+	/**
+	 * Move the Element to the upper Element (x, y+1) and replace old Element by an Empty Element.
+	 * 
+	 * @param movingElement is the type of Element which is moving
+	 */
+	protected void MoveLeft(MobileElement movingElement) {
+		
+	}
+	
+
+}
