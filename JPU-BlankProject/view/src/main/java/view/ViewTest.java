82d062a3f0d96727a3365ef9d23a60881897cd51
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2019-06-03 16:39:10 +0200

    Update ViewTest

---
+++ JPU-BlankProject/view/src/main/java/view/ViewTest.java
@@ -0,0 +1,84 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testView() {
+		
+	}
+
+	@Test
+	public void testRun() {
+		
+	}
+
+	@Test
+	public void testGetKeyPressed() {
+		
+	}
+
+	@Test
+	public void testSetKeyPressed() {
+		
+	}
+
+	@Test
+	public void testKeyPressed() {
+		
+	}
+
+	@Test
+	public void testKeyReleased() {
+		
+	}
+
+	@Test
+	public void testKeyTyped() {
+		
+	}
+
+	@Test
+	public void testGetFrameHeight() {
+		
+	}
+
+	@Test
+	public void testSetFrameHeight() {
+		
+	}
+
+	@Test
+	public void testGetFrameWidth() {
+		
+	}
+
+	@Test
+	public void testSetFrameWidth() {
+		
+	}
+
+}
